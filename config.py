3bec6a89393311c5fed4c6922084d16a7a53517f
plerros <48536508+plerros@users.noreply.github.com>
2026-09-22 06:01:05 +0300

    Metric targets update
    Revised my methodology for producing this set of numbers

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ class metric():
 	def get_target(self, hq):
 		return self.hq[1] if hq else self.default[1]
 class filter_t(metric):
-	def __init__(self, below_discard, below_discard_hq, contribution=0, power_mean=-1000):
+	def __init__(self, below_discard, below_discard_hq, contribution=0, power_mean=-5000):
 		super().__init__([below_discard]*3, [below_discard_hq]*3, contribution, power_mean)
 class target_t(metric):
-	def __init__(self, target, target_hq, contribution=1, power_mean=-1000):
+	def __init__(self, target, target_hq, contribution=1, power_mean=-5000):
 		super().__init__([target-0.1, target, target+5.0], [target_hq-0.1, target_hq, target_hq+5.0], contribution, power_mean)
 
 class CONFIG:
@@ -33,11 +33,11 @@ class CONFIG:
 	# All filters operate by computing a mean of all channels (RGB / etc)
 	# The per-frame means are then averaged using power mean
 	# Filters are cutoff values. Meet or beat to pass.
-	PSNR        = filter_t(30, 56)
-	SSIM_DB     = filter_t(17, 25)
-	PSNR_HVS_CB = filter_t(30, 56)
+	PSNR        = filter_t(50.33, 50.33)
+	SSIM_DB     = filter_t(21.27, 30.95)
+	PSNR_HVS_CB = filter_t(49.87, 55.12)
 	# Targets allow for values near them
-	VMAF_DB     = target_t(15, 17)
+	VMAF_DB     = target_t(12.91, 15.25)
 
 	# VAAV1_RESOLUTION_MODULO:
 	# If hardware only supports multiples of VAAV1_RESOLUTION_MODULO
